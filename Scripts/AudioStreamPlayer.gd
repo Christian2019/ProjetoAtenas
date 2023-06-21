90ee6a2df94fc17b82ebd16df9900805b84c50e3
Christian2019 <chrisschmidt2702@gmail.com>
2023-06-21 00:48:16 -0300

    UpdateMusic

--- Scripts/AudioStreamPlayer.gd
+++
@@ -1,8 +0,0 @@
-extends AudioStreamPlayer
-
-func _ready():
-	play(random(0,stream.get_length()))
-
-func random(min,max):
-	var rng = RandomNumberGenerator.new()
-	return int (rng.randf_range(min, max))
---
+++ Scripts/BackgroundMusic.gd
@@ -0,0 +1,10 @@
+extends AudioStreamPlayer
+
+func _ready():
+	var startTime = random(0,stream.get_length())
+	play(startTime)
+	print("Musica tempo de inicio: ",startTime,"s")
+
+func random(min,max):
+	var rng = RandomNumberGenerator.new()
+	return int (rng.randf_range(min, max))
