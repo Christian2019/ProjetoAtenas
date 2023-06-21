ca5570a0b76baa9a7e956b038a22145161362c8f
Christian2019 <chrisschmidt2702@gmail.com>
2023-06-21 01:51:59 -0300

    movimentacao e animacao basica personagem

---
+++ Scripts/player.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var speed = 2
+
+func _ready():
+	$Animation.play("Right")
+
+
+
+func _process(delta):
+	animationController()
+	moveController()	
+
+func animationController():
+	if Input.is_action_pressed("Move_Down"):
+		$Animation.play("Down")
+	elif Input.is_action_pressed("Move_Up"):
+		$Animation.play("Up")
+	elif Input.is_action_pressed("Move_Right"):
+		$Animation.play("Right")
+	elif Input.is_action_pressed("Move_Left"):
+		$Animation.play("Left")
+	else:
+		$Animation.stop()
+		
+		
+	
+func moveController():
+	if Input.is_action_pressed("Move_Down"):
+		position.y+=speed
+	elif Input.is_action_pressed("Move_Up"):
+		position.y-=speed
+	elif Input.is_action_pressed("Move_Right"):
+		position.x+=speed
+	elif Input.is_action_pressed("Move_Left"):
+		position.x-=speed
