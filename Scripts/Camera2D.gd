80f2daf51466df77a4cceb69189db187e59549ca
Christian2019 <chrisschmidt2702@gmail.com>
2023-06-23 02:44:26 -0300

    HUD e viewport

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -1,5 +1,9 @@
 extends Camera2D
 
+var mapFinalXPosition = 2555
+
+var mapFinalYPosition = 2757
+
 
 func _ready():
 	print(get_viewport().size)
@@ -10,7 +14,8 @@ func _process(delta):
 	moveCamera()
 	
 func moveCamera():
-	var playerPosition = get_parent().get_node("Player").position
-	var positionX= clamp(playerPosition.x,get_viewport().size.x/2,2555-get_viewport().size.x/2)
-	var positionY= clamp(playerPosition.y,get_viewport().size.y/2,2757-get_viewport().size.y/2)
+	
+	var playerPosition = get_parent().get_node("Game").get_node("Player").position
+	var positionX= clamp(playerPosition.x,(get_parent().size.x/zoom.x)/2,mapFinalXPosition-(get_parent().size.x/zoom.x)/2)
+	var positionY= clamp(playerPosition.y,(get_parent().size.y/zoom.y)/2,mapFinalYPosition-(get_parent().size.y/zoom.y)/2)
 	position= Vector2(positionX,positionY)
