ddf393900fd8a2323931bb64812f71e55a5e4a11
Christian2019 <chrisschmidt2702@gmail.com>
2023-12-02 15:10:32 -0300

    dragao add

--- Scripts/Game/Waves/Wave20.gd
+++ Scripts/Game/Waves/Wave20.gd
@@ -14,7 +14,7 @@ func _process(delta):
 	
 func waveBehavior():
 	#spawn(1*60,1,PreLoads.targetDummy,false)
-	spawn(1*60,5,PreLoads.id014,false)
+	spawn(1*60,5,PreLoads.id008,false)
 	#allEnemies()
 			
 func allEnemies():
